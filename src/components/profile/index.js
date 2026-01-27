9977d659ad04a4ba3041a55ec9906775757d28e1
Liran Naim <liran1305@gmail.com>
2026-01-27 15:34:42 +0200

    Add profile components index.js exports

---
+++ src/components/profile/index.js
@@ -0,0 +1,10 @@
+// Profile components exports
+export { default as DimensionCard } from './DimensionCard';
+export { default as KeyMetrics } from './KeyMetrics';
+export { default as QualitativeBadge } from './QualitativeBadge';
+export { default as StrengthTagsDisplay } from './StrengthTagsDisplay';
+export { default as NeverWorryAbout } from './NeverWorryAbout';
+export { default as ColleagueQuotes } from './ColleagueQuotes';
+export { default as RoomToGrow } from './RoomToGrow';
+export { default as WaitingState } from './WaitingState';
+export { default as ConsentModal } from './ConsentModal';
